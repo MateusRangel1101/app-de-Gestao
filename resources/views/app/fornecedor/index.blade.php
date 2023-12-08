f2ccd62269190c496c4d7de9be32b6dfa476454f
Mateus Rangel <mateusrangel.com.br@gmail.com>
2023-12-08 18:21:51 -0300

    if e else com blade

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -7,7 +7,7 @@
 {{-- @dd($fornecedores) --}}
 
 @if (count($fornecedores) >0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados</h3>
+    <h4>Existem alguns fornecedores cadastrados</h4>
 @elseif (count(fornecedores)>=10)
     <h4>Existem vários fornecedores cadastrados</h4>
 @else
