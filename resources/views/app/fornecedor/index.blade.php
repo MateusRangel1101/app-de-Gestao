f7fd1043c3e6999f04db096aea70f8b51f87c829
Mateus Rangel <mateusrangel.com.br@gmail.com>
2023-12-08 17:58:31 -0300

    Criando comentários com Blade e blocos PHP

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,12 @@
+<h3>Fornecedor View</h3>
+
+
+<!-- Testando comentario -->
+{{-- comentario no formato Blade --}}
+
+@php
+//Comentario no formato PHP_ROUND_HALF_DOWN
+/*
+Comentário em mais de uma linha
+*/
+@endphp
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 //     return 'HOME';
 // });
 
-Route::GET('/', 'PrincipalCOntroller@principal')->name('site.index');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
 
 Route::get('/sobrenos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 
@@ -32,9 +32,7 @@ Route::prefix('/app')->group(function () {
     Route::get('/clientes', function () {
         return 'Clientes';
     })->name('app.clientes');
-    Route::get('/fornecedores', function () {
-        return 'Fornecedores';
-    })->name('app.fornecedores');
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
     Route::get('/produtos', function () {
         return 'Produtos';
     })->name('app.produtos');
